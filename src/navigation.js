9105f01b7f771ddbe02dc2c7d406d76888255847
RAIRprotocol <73792201+rairprotocol@users.noreply.github.com>
2024-03-28 14:02:39 -0500

    clean up nav

--- src/navigation.js
+++ src/navigation.js
@@ -6,19 +6,11 @@ export const headerData = {
       text: 'Learn',
       links: [
         {
-          text: 'Features (Anchor Link)',
+          text: 'Features',
           href: getPermalink('/#features'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
+          text: 'About',
           href: getPermalink('/about'),
         },
         {
@@ -36,8 +28,8 @@ export const headerData = {
       ],
     },
     {
-      text: 'Telegram',
-      href: '#',
+      text: 'Follow us on X',
+      href: 'twitter.com/rairtech',
     },
   ],
   actions: [{ text: 'Github', href: 'https://github.com/rairprotocol', target: '_blank' }],
@@ -46,25 +38,12 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
       title: 'Platform',
       links: [
         { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'Github', href: '#' },
+        { text: 'Etherscan verify', href: '#' },
+        { text: 'About', href: '#' },
       ],
     },
     {
@@ -72,21 +51,7 @@ export const footerData = {
       links: [
         { text: 'Docs', href: '#' },
         { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
         { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
       ],
     },
   ],
@@ -96,10 +61,8 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Telegram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
