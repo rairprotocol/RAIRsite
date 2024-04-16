c85561e19aabc9a70c95b133d74db6419b9bd3c1
RAIRprotocol <73792201+rairprotocol@users.noreply.github.com>
2024-04-16 12:42:16 -0500

    new new copy

--- src/navigation.js
+++ src/navigation.js
@@ -6,8 +6,24 @@ export const headerData = {
       text: 'Learn',
       links: [
         {
-          text: 'Docs',
+          text: 'Knowledge Base',
           href: 'https://docs.rairprotocol.org',
+        },
+        {
+          text: 'API Reference',
+          href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rair-api',
+        },
+        {
+          text: 'Smart Contracts',
+          href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rairdapp/rairsolidity',
+        },
+        {
+          text: 'Whitepaper',
+          href: 'https://docs.rairprotocol.org/rairprotocol/whitepaper/introduction',
+        },
+        {
+          text: 'Tokenomics',
+          href: 'https://docs.rairprotocol.org/rairprotocol/tokenomics/overview',
         },
         {
           text: 'Terms',
@@ -24,7 +40,7 @@ export const headerData = {
       href: getPermalink('/contact'),
     },
   ],
-  actions: [{ text: 'Demo', href: 'https://rair.market', target: '_blank' }],
+  actions: [{ text: 'See Live Demo!', href: 'https://rair.market', target: '_blank' }],
 };
 
 export const footerData = {
@@ -32,18 +48,19 @@ export const footerData = {
     {
       title: 'Platform',
       links: [
-        { text: 'Developer API', href: 'https://docs.rairprotocol.org/rair-technologies/api/rair-api' },
         { text: 'Github', href: 'https://github.com/rairprotocol' },
-        { text: 'Smart contacts verify', href: '#' },
-        { text: 'Contact', href: 'https://rairprotocol.org/contact' },
+        { text: 'API Reference', href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rair-api' },
+        { text: 'Smart Contracts', href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rairdapp/rairsolidity' },
+        { text: 'Whitepaper', href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rairdapp/rairsolidity' },
+        { text: 'Tokenomics', href: 'https://docs.rairprotocol.org/rairprotocol/codebase/rairdapp/rairsolidity' },
       ],
     },
     {
       title: 'Support',
       links: [
         { text: 'Docs', href: 'https://docs.rairprotocol.org' },
-        { text: 'Community Forum', href: 'https://twitter.com/rairtech' },
-        { text: 'Status', href: 'https://twitter.com/rairtech' },
+        { text: 'Community Forum', href: 'https://discord.com/login?redirect_to=%2Fchannels%2F940249331766788168%2F973582780485234798' },
+        { text: 'Status', href: 'https://twitter.com/rairprotocol' },
       ],
     },
   ],
@@ -52,9 +69,9 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: '#' },
-    { ariaLabel: 'Linkedin', icon: 'tabler:brand-github', href: 'https://github.com/rairprotocol' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/rairprotocol' },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://discord.com/login?redirect_to=%2Fchannels%2F940249331766788168%2F973582780485234798' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/rairtech/' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
